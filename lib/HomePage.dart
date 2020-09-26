fc98912e50ee3807e996dcd517e599ca65d39b2d
MuhamadHaydar <49116664+MuhamadHaydar@users.noreply.github.com>
2020-09-26 22:54:05 +0300

    Add navigation drawer to HomePage.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -11,8 +11,68 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Blog App"),
-        backgroundColor:  Colors.redAccent,
+        backgroundColor: Colors.redAccent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
       ),
-    )
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Muhamad Haydar'),
+              accountEmail: Text('muhamadhaydarjawad@gmail.com'),
+              decoration: BoxDecoration(color: Colors.purple),
+            ),
+            ListTile(
+              title: Text("First Page"),
+              leading: Icon(Icons.cake, color: Colors.purple),
+            ),
+            ListTile(
+              title: Text('Second Page'),
+              leading: Icon(
+                Icons.search,
+                color: Colors.redAccent,
+              ),
+            ),
+            ListTile(
+              title: Text('Third Page'),
+              leading: Icon(
+                Icons.cached,
+                color: Colors.orange,
+              ),
+            ),
+            ListTile(
+              title: Text('Fourth Page'),
+              leading: Icon(
+                Icons.menu,
+                color: Colors.greenAccent,
+              ),
+            ),
+            Divider(
+              height: 10.0,
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text('Fourth Page'),
+              trailing: Icon(
+                Icons.close,
+                color: Colors.red,
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc_app/HomePage.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home:HomePage() ,
-  ));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
